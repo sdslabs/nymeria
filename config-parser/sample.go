a657c584353fb1ddc1a383fbf567de6d61437acb
aarya <aaryaag03@gmail.com>
2022-08-19 01:05:07 +0530

    config-parser

---
+++ config-parser/sample.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+  )
+
+func main() {
+	v :=viper.New()
+	v.SetConfigFile("parser.yaml")
+	v.ReadInConfig()
+	fmt.Println(v.GetString("url.frontend_url"))
+	fmt.Println(v.GetString("url.backend_url"))
+
+	
+}
