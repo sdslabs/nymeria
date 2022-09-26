3baa59090b228e7f13fede8fb0af561736e1f424
aarya <aaryaag03@gmail.com>
2022-09-26 17:09:09 +0530

    another format

--- parser.go
+++ parser.go
@@ -2,14 +2,35 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"github.com/spf13/viper"
-  )
+	"gopkg.in/yaml.v2"
+)
+
+type config struct {
+	url struct  {
+		frontend_url string `yaml:"frontend_url"`;
+		backend_url string `yaml:"backend_url"`;
+	} `yaml:"url"`
+}
 
 func main() {
-	v :=viper.New()
-	v.SetConfigFile("parser.yaml")
-	v.ReadInConfig()
-	fmt.Println(v.GetString("url.frontend_url"))
-	fmt.Println(v.GetString("url.backend_url"))
+	vi := viper.New()
+	vi.SetConfigFile("config.yaml")
+	vi.ReadInConfig()
+	fmt.Println(vi.GetString("url.frontend_url"))
+	
+	C := &config{}
+	source,err1 := ioutil.ReadFile("config.yaml")
+	if err1 != nil {
+		fmt.Println(err1)
+	}
+	
+	err := yaml.Unmarshal([]byte(source),&C)
+	if err != nil {
+		fmt.Println("unable to decode into struct, %v", err)
+	}
+
+	fmt.Println(C.url.backend_url)
 
 }
