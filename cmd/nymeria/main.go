d0b21859e3b773d3014f377ca8dfa630c7b5c4a4
Rohith Varma Buddaraju <rohith.varma.buddaraju@gmail.com>
2022-10-01 00:23:24 +0530

    Restore Main code

--- cmd/nymeria/main.go
+++ cmd/nymeria/main.go
@@ -1,23 +1,9 @@
 package main
 
-// import (
-// 	"github.com/sdslabs/nymeria/api"
-// )
-
-// func main() {
-// 	api.Start()
-// }
-
 import (
-	"fmt"
-
-	"github.com/sdslabs/nymeria/config"
+	"github.com/sdslabs/nymeria/api"
 )
 
 func main() {
-	var c config.Config
-	c.GetConf()
-
-	fmt.Println(c.Url.Frontend_url)
-	fmt.Println(c.Url.Backend_url)
+	api.Start()
 }
