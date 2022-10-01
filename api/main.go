8b13c47e3058021e593e15122b02a3b683199c82
reeshi-18 <reeshipaul35@gmail.com>
2022-10-02 00:10:51 +0530

    login changes

--- api/main.go
+++ api/main.go
@@ -4,21 +4,11 @@ import (
     "github.com/gin-gonic/gin"
 	m"github.com/sdslabs/nymeria/pkg/middleware"
 	c"github.com/sdslabs/nymeria/pkg/controller/admin"
-<<<<<<< HEAD
-=======
     l"github.com/sdslabs/nymeria/pkg/controller/login"
->>>>>>> bea42114aa1a480459ebcccc6109d01ffca3d767
 )
 
 
 func Start() {
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-	r.GET("/register", controller.Registration)
-=======
-=======
->>>>>>> bea42114aa1a480459ebcccc6109d01ffca3d767
     r := gin.Default()
     k := m.NewMiddleware()
 
@@ -30,20 +20,6 @@ func Start() {
         })
     })
 
-<<<<<<< HEAD
-	//r.POST("/create-identity", c.CreateIdentity)
-    r.POST("/create-identity", c.CreateIdentity)
-	r.GET("/get-identity", c.GetIdentity)
-	r.POST("/delete-identity", c.DeleteIdentity)
-
-	
-	r.Run()
-<<<<<<< HEAD
->>>>>>> e06a244 (Added identities controllers)
-
-=======
->>>>>>> a204a8e (implemented delete identities controller)
-=======
     r.POST("/create-identity", c.CreateIdentity)
 	r.GET("/get-identity", c.GetIdentity)
 	r.POST("/delete-identity", c.DeleteIdentity)
@@ -51,6 +27,5 @@ func Start() {
 
 	
 	r.Run()
->>>>>>> bea42114aa1a480459ebcccc6109d01ffca3d767
     // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
--- pkg/controller/login/login.go
+++ pkg/controller/login/login.go
@@ -11,7 +11,7 @@ import (
 func InitializeSelfServiceLoginFlow(c *gin.Context) {
     refresh := true // bool | Refresh a login session  If set to true, this will refresh an existing login session by asking the user to sign in again. This will reset the authenticated_at time of the session. (optional)
     aal := "aal1" // string | Request a Specific AuthenticationMethod Assurance Level  Use this parameter to upgrade an existing session's authenticator assurance level (AAL). This allows you to ask for multi-factor authentication. When an identity sign in using e.g. username+password, the AAL is 1. If you wish to \"upgrade\" the session's security by asking the user to perform TOTP / WebAuth/ ... you would set this to \"aal2\". (optional)
-    returnTo := "http://127.0.0.1:4434" // string | The URL to return the browser to after the flow was completed. (optional)
+    returnTo := "" // string | The URL to return the browser to after the flow was completed. (optional)
 
     configuration := client.NewConfiguration()
     apiClient := client.NewAPIClient(configuration)
@@ -23,4 +23,8 @@ func InitializeSelfServiceLoginFlow(c *gin.Context) {
     }
     // response from `InitializeSelfServiceLoginFlowForBrowsers`: SelfServiceLoginFlow
     fmt.Fprintf(os.Stdout, "Response from `V0alpha2Api.InitializeSelfServiceLoginFlowForBrowsers`: %v\n", resp)
+
+    //Id := resp.Id
+    //Cookie := r.Header.Get("Set-Cookie")
+    //GetLoginFlow(Id,Cookie)
 }
